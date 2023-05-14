4edc7b959b612c2144251212fe6e4ca62c89cfe9
Potter <potteramylynn@gmail.com>
2023-05-14 13:32:58 -0500

    started readme.md and wrote list of questions in index.js

---
+++ README.md
@@ -0,0 +1,10 @@
+# Professional README generator
+## About
+In this project I created a professional README generator using node. The user answers prompts on the command line and are given a README.md file with headers for the title and different categories and their provided information filled in each section.
+## Installation
+This application requires the installation of Node.js and Inquirer v8.2.4 and is run from the command line.
+
+## Usage
+Use this application to easily create professional README.md for your repositoriy
+## Screenshots
+## Link to Video Demonstration
--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const {buildPageTitle, buildTableOfContents} = require("./generate")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    "What is the title of your project?"
+  }
+  {
+    "Write a description of your project."
+  }
+  {
+    "Describe how to install your project."
+  }
+  {
+    "How should a user use your project?"
+  }
+  {
+    "What license would you like to add to your project?"
+  }
+  {
+    "Write your contibution guidelines."
+  }
+  {
+    "If you wrote any tests for your file, please include usage instructions here."
+  }
+  {
+    "What is your GitHub username?"
+  }
+  {
+    "What is your email address?"
+  }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile('README.md')
+}
 
 // TODO: Create a function to initialize app
 function init() {}
