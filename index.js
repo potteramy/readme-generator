3786de5a3d46b789d6cf8400962d9f3cb6cf4d18
Potter <potteramylynn@gmail.com>
2023-05-17 13:18:12 -0500

    added to generate markdow function

--- index.js
+++ index.js
@@ -1,39 +1,72 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 const {buildPageTitle, buildTableOfContents} = require("./generate")
 // TODO: Create an array of questions for user input
-const questions = [
+// const questions = [
+  inquirer
+    .prompt([
   {
-    "What is the title of your project?"
-  }
+    type: 'input',
+    name: 'gitHub',
+    message: "What is your GitHub username?",
+  },
   {
-    "Write a description of your project."
-  }
+    type: 'input',
+    name: 'email',
+    message: "What is your email address?",
+  },
   {
-    "Describe how to install your project."
-  }
+    type: 'input',
+    name: 'title',
+    message: "What is the title of your project?",
+  },
   {
-    "How should a user use your project?"
-  }
+    type: 'input',
+    name: 'description',
+    message: "Please write a short description of your project.",
+  },
   {
-    "What license would you like to add to your project?"
-  }
+    type: 'list',
+    name: 'license',
+    message: "What license should your project have?",
+    choices:[
+    "None",
+    "Apache 2.0",
+    "GPLv2",
+    "GPLv3",
+    "MIT",
+    "CC0"
+    ],
+  },
   {
-    "Write your contibution guidelines."
-  }
+    type: 'input',
+    name: 'install',
+    message: "What command should be run to install dependencies?",
+  },
   {
-    "If you wrote any tests for your file, please include usage instructions here."
-  }
+    type: 'input',
+    name: 'tests',
+    message: "What command should be run to run tests?",
+  },
   {
-    "What is your GitHub username?"
-  }
+    type: 'input',
+    name: 'usage',
+    message: "What does the user need to know about using the repo?",
+  },
   {
-    "What is your email address?"
+    type: 'input',
+    name: 'contribution',
+    message: "What does the user need to know about contributing to the repo?",
   }
-];
+])
+
+// ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+  fileName = README.md
   fs.writeFile('README.md')
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,24 @@
+const fs = require("fs");
+
+function buildPageTitle(){
+
+};
+function buildTableOfContents(){
+
+};
+
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // "None" []
+  // "Apache" [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  // "GPLv2" [![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+  // "GPLv3" [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+  // "MIT" [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  // "CC0" [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,7 +32,38 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  -[Installation](#installation)
+  -[Usage](#Usage)
+  -[License](#License)
+  -[Contribution](#Contribution)
+  -[Tests](#Tests)
+  -[Questions](#Questions)
+
+  ## Installation
+  To install dependencies, enter in your command line: ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ## Contribution
+  ${data.contribution}
+
+  ## Tests
+  For testing, enter in your command line: ${data.tests}
+
+  ## Questions
+  If you have any questions, please contact ${data.gitHub} by email to ${data.email}`;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  buildPageTitle,
+  buildTableOfContents,
+  generateMarkdown
+}
